53f1c6e05de590c482bca375fabf221098b80dcd
Abhishek <abhi.singh15dec@gmail.com>
2025-08-26 23:08:13 +0530

    Add files via upload

--- Java/Loops.java
+++ Java/Loops.java
@@ -59,7 +59,7 @@ class Loops {
 }
 */
 
-
+/* 
 
 public class Loops {
     public static void main(String[] args) {
@@ -72,5 +72,28 @@ public class Loops {
         }
 
         System.out.println(fact);
+    }
+}
+*/
+
+
+
+class Loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            boolean prime = true;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
