d7f997fa7abf29f9c9db0bdfd9b8d27ee82c7477
Abhishek <abhi.singh15dec@gmail.com>
2025-09-13 23:18:57 +0530

    Add files via upload

--- Java/Patterns.java
+++ Java/Patterns.java
@@ -196,6 +196,7 @@ public static void main(String[] args) {
 }
 */
 
+/*
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -213,6 +214,26 @@ public static void main(String[] args) {
                 System.out.print(j);
             }
 
+            System.out.println();
+        }
+    }
+}
+*/
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
             System.out.println();
         }
     }
