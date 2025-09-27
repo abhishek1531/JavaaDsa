5d48b3c6db63c06517063f4ac23e918922bb2c82
Abhishek <abhi.singh15dec@gmail.com>
2025-09-27 23:13:33 +0530

    Add files via upload

--- Java/arrays.java
+++ Java/arrays.java
@@ -11,25 +11,54 @@
 //     }
 // }
 
+// import java.util.*;
+
+// class arrays {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();   
+//         int a[] = new int[n];
+
+//         for(int i = 0; i < n; i++) {
+//             a[i] = sc.nextInt();
+//         }
+
+//         int sum = 0;
+//         for(int i = 0; i < n; i++) {
+//             sum += a[i];         
+//         }
+
+//         System.out.print(sum);
+//     }
+// }
+
+
 import java.util.*;
 
 class arrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();   
+        int n = sc.nextInt();
         int a[] = new int[n];
 
         for(int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
 
-        int sum = 0;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
         for(int i = 0; i < n; i++) {
-            sum += a[i];         
+            if(a[i] > max1) {
+                max2 = max1;
+                max1 = a[i];
+            } else if(a[i] > max2 && a[i] != max1) {
+                max2 = a[i];
+            }
         }
 
-        System.out.print(sum);
+        System.out.print(max2);
     }
 }
-
