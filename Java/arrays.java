9dac8b76ca05b84950ee00cee95cbf67c5643642
Abhishek <abhi.singh15dec@gmail.com>
2025-09-28 23:17:23 +0530

    Add files via upload

--- Java/arrays.java
+++ Java/arrays.java
@@ -34,6 +34,35 @@
 // }
 
 
+// import java.util.*;
+
+// class arrays {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+//         int a[] = new int[n];
+
+//         for(int i = 0; i < n; i++) {
+//             a[i] = sc.nextInt();
+//         }
+
+//         int max1 = Integer.MIN_VALUE;
+//         int max2 = Integer.MIN_VALUE;
+
+//         for(int i = 0; i < n; i++) {
+//             if(a[i] > max1) {
+//                 max2 = max1;
+//                 max1 = a[i];
+//             } else if(a[i] > max2 && a[i] != max1) {
+//                 max2 = a[i];
+//             }
+//         }
+
+//         System.out.print(max2);
+//     }
+// }
+
 import java.util.*;
 
 class arrays {
@@ -47,18 +76,15 @@ class arrays {
             a[i] = sc.nextInt();
         }
 
-        int max1 = Integer.MIN_VALUE;
-        int max2 = Integer.MIN_VALUE;
-
+        int even = 0, odd = 0;
         for(int i = 0; i < n; i++) {
-            if(a[i] > max1) {
-                max2 = max1;
-                max1 = a[i];
-            } else if(a[i] > max2 && a[i] != max1) {
-                max2 = a[i];
+            if(a[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
             }
         }
 
-        System.out.print(max2);
+        System.out.print(even + " " + odd);  
     }
 }
